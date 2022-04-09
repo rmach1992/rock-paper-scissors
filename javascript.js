23b18ba50f27ad581f0f48002291f7f0d097d3e4
Raymond <rmach1992@gmail.com>
2022-04-08 22:47:50 -0700

    Add tracking of scores and displays overall winner

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
 //Select divs to display everything
 const results = document.querySelector('.results');
 const selection = document.querySelector('.selection');
+const score = document.querySelector('.score');
+const winner = document.querySelector('.winner');
+let playerWin = 0, computerWin = 0 
 
 //Function that will return either rock paper or scissors
 function computerPlay() {
@@ -20,24 +23,31 @@ function computerPlay() {
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         results.textContent = "Tie. Try Again"
+        return "Tie";
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
         results.textContent = "You Win! Rock beats Scissors";
+        return "You Win!"
     } 
     else if(playerSelection === "paper" && computerSelection === "rock") {
         results.textContent = "You Win! Paper beats Rock"
+        return "You Win!"
     }
     else if(playerSelection === "scissors" && computerSelection === "paper") {
         results.textContent = "You Win! Scissors beats Paper"
+        return "You Win!"
     }
     else if(playerSelection === "rock" && computerSelection === "paper") {
         results.textContent = "You Lose! Paper beats Rock"
+        return "You Lose!"
     }
     else if(playerSelection === "paper" && computerSelection === "scissors") {
         results.textContent = "You Lose! Scissors beats Paper"
+        return "You Lose!"
     }
     else if(playerSelection === "scissors" && computerSelection === "rock") {
         results.textContent = "You Lose! Rock beats Scissors"
+        return "You Lose!"
     }
 }
 
@@ -47,43 +57,44 @@ function showSelection(playerSelection, computerSelection){
 }
 
 //Function to track scores
-//function trackScores {
-//    let playerWin = 0, computerWin = 0   
-//    if ((playRound (playerSelection, computerSelection)).startsWith("You Lose!")) {
-//        computerWin += 1;
-//    }
-//    else if ((playRound (playerSelection, computerSelection)).startsWith("You Win!")) {
-//        playerWin += 1;
-//    }
-//    }
-//    if (playerWin > computerWin) {
-//        console.log ("You scored ", playerWin, " and the computer scored ", computerWin, ". You win.")
-//    } 
-//    else if (computerWin > playerWin) {
-//        console.log ("You scored ", playerWin, " and the computer scored ", computerWin, ". The computer wins.")
-//    }
-//    else console.log ("You scored ", playerWin, " and the computer scored ", computerWin, ". It is a tie.")
-//}
+function trackScores(playerSelection, computerSelection) {  
+    if ((playRound (playerSelection, computerSelection)).startsWith("You Lose!")) {
+        computerWin += 1;
+    }
+    else if ((playRound (playerSelection, computerSelection)).startsWith("You Win!")) {
+        playerWin += 1;
+    }
+    score.textContent = "The player has a score of " + playerWin + " and the computer has a score of " + computerWin + ".";
+    if (playerWin === 5) {
+        winner.textContent = "You scored 5 points first! You win!";
+    } 
+    else if (computerWin === 5) {
+        winner.textContent = "The computer scored 5 points first! You lose!";
+    }
+}
 
 //Event listener for the buttons
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', () => {
     const computerSelection = computerPlay();
     showSelection('rock', computerSelection);
-    playRound ('rock', computerSelection);
+    playRound('rock', computerSelection);
+    trackScores('rock', computerSelection);
 })
 
 const paper = document.querySelector('#paper');
 paper.addEventListener('click', () => {
     const computerSelection = computerPlay();
     showSelection('paper', computerSelection);
-    playRound ('paper', computerSelection);
+    playRound('paper', computerSelection);
+    trackScores('paper', computerSelection);
 })
 
 const scissors = document.querySelector('#scissors');
 scissors.addEventListener('click', () => {
     const computerSelection = computerPlay();
     showSelection('scissors', computerSelection);
-    playRound ('scissors', computerSelection);
+    playRound('scissors', computerSelection);
+    trackScores('scissors', computerSelection);
 })
 
