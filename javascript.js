f95435ee787c1afbc37458a06e9652a17f6f6faa
Raymond <rmach1992@gmail.com>
2022-04-08 22:13:14 -0700

    Add buttons and display results

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+const results = document.querySelector('.results');
+
 //Function that will return either rock paper or scissors
 function computerPlay() {
     let random = Math.floor(Math.random() * 3);
@@ -15,55 +17,66 @@ function computerPlay() {
 //Function to play a single round of rock paper scissors
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "Tie. Try Again"
+        results.textContent = "Tie. Try Again"
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You Win! Rock beats Scissors";
+        results.textContent = "You Win! Rock beats Scissors";
     } 
     else if(playerSelection === "paper" && computerSelection === "rock") {
-        return "You Win! Paper beats Rock"
+        results.textContent = "You Win! Paper beats Rock"
     }
     else if(playerSelection === "scissors" && computerSelection === "paper") {
-        return "You Win! Scissors beats Paper"
+        results.textContent = "You Win! Scissors beats Paper"
     }
     else if(playerSelection === "rock" && computerSelection === "paper") {
-        return "You Lose! Paper beats Rock"
+        results.textContent = "You Lose! Paper beats Rock"
     }
     else if(playerSelection === "paper" && computerSelection === "scissors") {
-        return "You Lose! Scissors beats Paper"
+        results.textContent = "You Lose! Scissors beats Paper"
     }
     else if(playerSelection === "scissors" && computerSelection === "rock") {
-        return "You Lose! Rock beats Scissors"
+        results.textContent = "You Lose! Rock beats Scissors"
     }
 }
 
 //Function to play a 5 round game
-function game(){
-    let playerWin = 0, computerWin = 0;
-    for (let i = 0; i < 5; i++){
-        const playerSelection = prompt("Please type in either rock, paper or scissors.").toLowerCase();
-        //if (playerSelection !== "rock" || playerSelection !== "paper" || playerSelection !== "scissors") {
-        //playerSelection = prompt("Invalid output. Please type in either rock, paper or scissors.")
-        //}
-        const computerSelection = computerPlay();
-        console.log ("You selected", playerSelection);
-        console.log ("The computer selected", computerSelection);
-        console.log(playRound (playerSelection, computerSelection));
-        
-        if ((playRound (playerSelection, computerSelection)).startsWith("You Lose!")) {
-            computerWin += 1;
-        }
-        else if ((playRound (playerSelection, computerSelection)).startsWith("You Win!")) {
-            playerWin += 1;
-        }
-    }
-    if (playerWin > computerWin) {
-        console.log ("You scored ", playerWin, " and the computer scored ", computerWin, ". You win.")
-    } 
-    else if (computerWin > playerWin) {
-        console.log ("You scored ", playerWin, " and the computer scored ", computerWin, ". The computer wins.")
-    }
-    else console.log ("You scored ", playerWin, " and the computer scored ", computerWin, ". It is a tie.")
-}
+//function game(){
+//    let playerWin = 0, computerWin = 0;
+//    const computerSelection = computerPlay();
+//    console.log ("You selected", playerSelection);
+//    console.log ("The computer selected", computerSelection);
+//    console.log(playRound (playerSelection, computerSelection));
+//        
+//    if ((playRound (playerSelection, computerSelection)).startsWith("You Lose!")) {
+//        computerWin += 1;
+//    }
+//    else if ((playRound (playerSelection, computerSelection)).startsWith("You Win!")) {
+//        playerWin += 1;
+//    }
+//    }
+//    if (playerWin > computerWin) {
+//        console.log ("You scored ", playerWin, " and the computer scored ", computerWin, ". You win.")
+//    } 
+//    else if (computerWin > playerWin) {
+//        console.log ("You scored ", playerWin, " and the computer scored ", computerWin, ". The computer wins.")
+//    }
+//    else console.log ("You scored ", playerWin, " and the computer scored ", computerWin, ". It is a tie.")
 
-console.log(game ());
+//console.log(game ());
+
+//Event listener for the buttons
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    playRound ('rock', computerPlay());
+})
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+    playRound ('paper', computerPlay());
+})
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+    playRound ('scissors', computerPlay());
+})
+
